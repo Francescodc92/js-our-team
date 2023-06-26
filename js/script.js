cc9df75ceb8e93ed70bc3d6e685365d39d054c30
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-06-26 19:22:11 +0200

    add console log member info, end milestone 1

--- js/script.js
+++ js/script.js
@@ -14,41 +14,55 @@ Barbara Ramos  | Graphic Designer      | barbara-ramos-graphic-designer.jpg
   - MILESTONE 0 :
     - Creare l'array di oggetti con le informazioni fornite.
 
+  - MILESTONE 1 :
+    - Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
 RAGIONAMENTO BASE MILESTONE 0
-1 reare un array di oggetti , ogni oggetto si riferirà a uno degli lementi del team 
+1 reare un array di oggetti , ogni oggetto si riferirà a uno degli lementi del team
 
-
+RAGIONAMENTO BASE MILESTONE 1
+1 creare un ciclo per percorrere l'array
+  - prendere le informazioni del membro del team contenute nell'oggetto nella posizione corrente dell'array
+    - stampare in console le informazioni  
 */
 
 const teamMembers = [
   {
-    name: 'Wayne Barnett',
+    memberName: 'Wayne Barnett',
     businessRole: 'Founder & CEO',
     photo: 'wayne-barnett-founder-ceo.jpg',
   },
   {
-    name: 'Angela Caroll',
+    memberName: 'Angela Caroll',
     businessRole: 'Chief Editor',
     photo: 'angela-caroll-chief-editor.jpg',
   },
   {
-    name: 'Walter Gordon',
+    memberName: 'Walter Gordon',
     businessRole: 'Office Manager',
     photo: 'walter-gordon-office-manager.jpg',
   },
   {
-    name: 'Angela Lopez',
+    memberName: 'Angela Lopez',
     businessRole: 'Social Media Manager',
     photo: 'angela-lopez-social-media-manager.jpg',
   },
   {
-    name: 'Scott Estrada',
+    memberName: 'Scott Estrada',
     businessRole: 'Developer',
     photo: 'scott-estrada-developer.jpg',
   },
   {
-    name: 'Barbara Ramos',
+    memberName: 'Barbara Ramos',
     businessRole: 'Graphic Designer',
     photo: 'barbara-ramos-graphic-designer.jpg',
   }
 ];
+
+teamMembers.forEach(element => {
+  const currentMemberName = element.memberName
+  const currentBusinessRole = element.businessRole
+  const currentMemberPhoto = element.photo
+
+  console.log('Nome membro: ' + currentMemberName, 'Ruolo membro: ' + currentBusinessRole,'link foto : ' +  currentMemberPhoto)
+})
