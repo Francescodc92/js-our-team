5c75814178afcfa37307324a946378ead404725c
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-06-26 19:05:02 +0200

    add array team info end milestone 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
-console.log("hello world")
+/*
+Dati: 
+Nome           | Ruolo                 | Immagine
+-------------- | --------------------- | ------------- 
+Wayne Barnett  | Founder & CEO         | wayne-barnett-founder-ceo.jpg
+Angela Caroll  | Chief Editor          | angela-caroll-chief-editor.jpg
+Walter Gordon  | Office Manager        | walter-gordon-office-manager.jpg
+Angela Lopez   | Social Media Manager  | angela-lopez-social-media-manager.jpg
+Scott Estrada  | Developer             | scott-estrada-developer.jpg
+Barbara Ramos  | Graphic Designer      | barbara-ramos-graphic-designer.jpg
+
+- Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+  - MILESTONE 0 :
+    - Creare l'array di oggetti con le informazioni fornite.
+
+RAGIONAMENTO BASE MILESTONE 0
+1 reare un array di oggetti , ogni oggetto si riferirà a uno degli lementi del team 
+
+
+*/
+
+const teamMembers = [
+  {
+    name: 'Wayne Barnett',
+    businessRole: 'Founder & CEO',
+    photo: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    businessRole: 'Chief Editor',
+    photo: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    businessRole: 'Office Manager',
+    photo: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    businessRole: 'Social Media Manager',
+    photo: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    businessRole: 'Developer',
+    photo: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    businessRole: 'Graphic Designer',
+    photo: 'barbara-ramos-graphic-designer.jpg',
+  }
+];
